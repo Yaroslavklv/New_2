b7a9ee32d74956be37d25391bcb43a8e647ab235
yaroslavklv <yaroslavklv@gmail.com>
2022-01-08 18:12:37 +0200

    working with strings

--- lesson4.py
+++ lesson4.py
@@ -1,3 +1,9 @@
-#a=input()
 b='Это строка в которую {a} новую строку'.format(a=input())
 print(b)
+b='Это строка в которую {a} новую строку'.format(a='замена в строке')
+print(b)
+print(len(b))
+if b.count('строка'):
+    print('Да')
+else:
+    print('НЕТ')
