28dd6e27ba80ff5e9c07af597d0502f91b8a087c
yaroslavklv <yaroslavklv@gmail.com>
2022-01-08 14:30:05 +0200

    cycle replacement

--- lesson_3.py
+++ lesson_3.py
@@ -1,7 +1,9 @@
 first_number = int(input("enter a three-digit integer"))
 
-b1=first_number%10
-intermediate_number=first_number//10
-b2=intermediate_number%10
-b3=intermediate_number//10
-print(b1,b2,b3)
+additional_number=0
+additional_number=additional_number*10+first_number%10
+first_number=first_number//10
+additional_number=additional_number*10+first_number%10
+first_number=first_number//10
+additional_number=additional_number*10+first_number
+print(additional_number)
