70650bf91c45d2bdceea06710a4d1c5763eec8e0
yaroslavklv <yaroslavklv@gmail.com>
2022-01-16 15:55:21 +0200

    List task_1_isn'treplacement

--- lesson6_1.py
+++ lesson6_1.py
@@ -1,16 +1,23 @@
 import random
 
 # a=[random.randint(150,200) for _ in range(10)]
-a = [180, 167, 155, 178, 180, 190, 200]
+a = [181, 167, 155, 178, 182, 190, 200]
 x = int(input())
 print(a)
 a.append(x)
 k = a.count(x)
-# if k>1:
-
 a.sort(reverse=True)
-
 t = a.index(x)
+print(a, k, "index_", t+1)
 
-b = t + 1
-print(a, k, "index_", b)
+
+#if k>1:
+#   a.insert((a.index(x)-k),x)
+
+#else:
+
+
+
+#a.pop(t-1)
+#b = t + 1
+
