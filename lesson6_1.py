17684ded714ec49ab360eb426f9229b2e3d51c95
yaroslavklv <yaroslavklv@gmail.com>
2022-01-16 18:16:44 +0200

    working with lists_lesson_6

--- lesson6_1.py
+++ lesson6_1.py
@@ -1,23 +1,6 @@
 import random
-
-# a=[random.randint(150,200) for _ in range(10)]
-a = [181, 167, 155, 178, 182, 190, 200]
-x = int(input())
-print(a)
+a=[random.randint(120,200) for _ in range(10)]
+x=int(input())
 a.append(x)
-k = a.count(x)
 a.sort(reverse=True)
-t = a.index(x)
-print(a, k, "index_", t+1)
-
-
-#if k>1:
-#   a.insert((a.index(x)-k),x)
-
-#else:
-
-
-
-#a.pop(t-1)
-#b = t + 1
-
+print(a.index(x)+a.count(x))
