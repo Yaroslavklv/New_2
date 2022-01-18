9d740e876653c161a229a9db107fab82cb95e3a6
yaroslavklv <yaroslavklv@gmail.com>
2022-01-18 17:57:27 +0200

    lesson_8_it's_unique_numbers_2

--- lesson_8.py
+++ lesson_8.py
@@ -1,7 +1,7 @@
 import random
 a=[random.randint(1,20) for _ in range(10)]
 b=[random.randint(1,20) for _ in range(10)]
-c=(a+b)
-c.sort()
-c=[ch for ch in range(0,len(c)-1) if c[ch]!=c[ch-1] and c[ch]!=c[ch+1]]
-print(len(c))
+a=a+b
+a.sort()
+a=[ch for ch in range(0,len(a)-1) if a[ch]!=a[ch-1] and a[ch]!=a[ch+1]]
+print(len(a))
