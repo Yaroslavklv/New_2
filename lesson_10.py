e4281147e861c8f36b18d65156f69b54b228ca84
yaroslavklv <yaroslavklv@gmail.com>
2022-01-21 22:55:57 +0200

    lesson_10_dictionaries_2_3

--- lesson_10.py
+++ lesson_10.py
@@ -1,4 +1,19 @@
 import random, string
 dict_1=dict(zip([random.choice(string.ascii_letters) for _ in range(10)],[random.randint(1,50) for _ in range(10)]))
+print(dict_1)
 dict_2=dict(zip([random.choice(string.ascii_letters) for _ in range(10)],[random.randint(1,50) for _ in range(10)]))
-print(dict_1, dict_2)
+print(dict_2)
+dict_3={**dict_1,**dict_2}
+print(dict_3)
+
+#for key, value in dict_1.items():
+#    for key, value in dict_1.items():
+#        dict_3[]=
+ #       print (key)
+#             else:
+#                 dict_3.update(key=h)
+#         else:
+#             if dict_1[key]!=dict_2[i]:
+#                 dict_3.update(key=value)
+#             else:
+#                 dict_3.update(i=h)
