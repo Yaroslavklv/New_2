a7fbe968b36d2405df20d39204e41b271bd97677
yaroslavklv <yaroslavklv@gmail.com>
2022-01-23 19:58:20 +0200

    lesson_13_functions_13_Function for finding algebraic expressions for a quare

---
+++ lesson_13.py
@@ -0,0 +1,13 @@
+def square (a):
+    """
+    Function for finding algebraic expressions for a square: the perimeter of the square,
+    the area of the square and the diagonal of the square
+    :type x:int
+    :param a: side of a square
+    :return: calculation result: perimeter, area, square diagonal
+    """
+    perimeter=a*4
+    area=a*a
+    diagonal=a*(2**(1/2))
+    return  (perimeter, area, diagonal)
+print(square(int(input())))
