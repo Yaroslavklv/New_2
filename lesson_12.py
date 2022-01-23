b181da333b31f9fd9f6256dc188898c96e83b7e0
yaroslavklv <yaroslavklv@gmail.com>
2022-01-23 19:03:37 +0200

    lesson_12_functions_2_added description

--- lesson_12.py
+++ lesson_12.py
@@ -1,4 +1,10 @@
-def function_remainder(x):
+def multiplication_by_number(x):
+    """
+    The function divides the number by the bit depth and multiplies each bit length of the number among themselves
+    :type x:int
+    :param x: positive integer
+    :return: calculation result
+    """
     temp=1
     while x!=0:
         t = x % 10
