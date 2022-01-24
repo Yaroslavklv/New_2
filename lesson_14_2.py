efe2f3e44f06cb49c7220c75fd2cdd83aacce626
yaroslavklv <yaroslavklv@gmail.com>
2022-01-24 15:42:25 +0200

    lesson_14_2_function_arithmetic

---
+++ lesson_14_2.py
@@ -0,0 +1,11 @@
+def arithmetic (a,b,c):
+
+a=int(input())
+b=int(input())
+c=input()
+alg={"+":a+b, "-":a-b, "*":a*b, "/":a/b}
+if alg.get(c):
+    print(alg[c])
+else:
+    print("Неизвестная операция")
+return alg[c]
