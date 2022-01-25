13349c9c5c429d5e46fd5c43e0d2b1e9eac7814f
yaroslavklv <yaroslavklv@gmail.com>
2022-01-25 12:55:02 +0200

    lesson_14_3_function_arithmetic_after remarks

---
+++ 11111111111.py
@@ -0,0 +1,4 @@
+a=12345
+
+for i in range(1,10):
+  print (i)
--- lesson_14_2.py
+++ lesson_14_2.py
@@ -1,22 +1,15 @@
 def arithmetic (a,b,c):
-"""
-Nev function performs algebraic operations on argument 1 and argument 2, depending on the specified operation:
-addition, subtraction, multiplication and division.
-If another operation is entered, then the line "Unknown operation" is displayed.
-:type a:int
-:type b:int
-:tepe c:str
-:param a: argument 1
-:param b: argument 2
-:param c: specified operation
-:return int and str the value of the operation performed on arguments 1 and 2
-"""
-a=int(input())
-b=int(input())
-c=input()
-alg={"+":a+b, "-":a-b, "*":a*b, "/":a/b}
-if alg.get(c):
-    print(alg[c])
-else:
-    print("Неизвестная операция")
-return alg[c], print("Неизвестная операция")
+ '''
+ Nev function performs algebraic operations on argument 1 and argument 2, depending on the specified operation:
+ addition, subtraction, multiplication and division.
+ If another operation is entered, then the line "Unknown operation" is displayed.
+ :type a:int
+ :type b:int
+ :tepe c:str
+ :param a: argument 1
+ :param b: argument 2
+ :param c: specified operation
+ :return int and str the value of the operation performed on arguments 1 and 2
+ '''
+ alg={"+":a+b, "-":a-b, "*":a*b, "/":a/b}
+ return alg.get(c,"Неизвестная операция")
