dc9b601741e5156964e921b86e87967865724005
yaroslavklv <yaroslavklv@gmail.com>
2022-01-28 17:42:32 +0200

    lesson_15

---
+++ lesson_15.py
@@ -0,0 +1,23 @@
+def check_password(*args, **kwargs):
+    def wrapper():
+
+
+    return any(*args, **kwargs)
+return wrapper
+
+def authenticate():
+    def wrapper():
+
+
+    return
+return wrapper
+
+
+
+
+@check_password
+@authenticate
+def login(username,password):
+    return True
+
+
