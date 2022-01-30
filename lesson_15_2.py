248a2dd71b3afcb028414bc3245cdc32aa91c90b
yaroslavklv <yaroslavklv@gmail.com>
2022-01-30 13:44:46 +0200

    lesson_15_2_decorator_2

--- lesson_15_2.py
+++ lesson_15_2.py
@@ -1,15 +1,37 @@
 
 def check_password(func: object) -> object:
     """
-
+    The function checks for the presence of a login in the global dictionary
     :type func: object
+    :return: true or false
     """
     def wrapper(username, password):
         return any(key for key, value in temp.items() if key==username and value==password)
 
     return wrapper
 
+def authenticate(func_2):
+    """
+    User authentication function
+    :param func_2:
+    :return:
+    """
 
+    def wrapper(username, password):
+        if any(key for key, value in temp.items() if key==username and value==password) == True:
+            return f'Вы в системе!'
+        else:
+            n=1
+            while n<3:
+                n += 1
+                return f'Не правильное Имя или Пароль. У вас осталось {n} попыток'
+            else:
+                return f'Попытки истекли'
+
+    return wrapper
+
+
+@authenticate
 @check_password
 def orig(username, password):
  return True
@@ -17,4 +39,4 @@ def orig(username, password):
 if __name__ == '__main__':
     temp = {'yara': '13444!', 'dav': '2345v'}
 
-    print(orig('yaa', '13444!'))
+    print(orig('y', '13444!'))
